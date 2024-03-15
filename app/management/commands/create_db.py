2bf9754882aec0a02c3adef368a99ed1704613ea
LatherioK0818 <80198308+LatherioK0818@users.noreply.github.com>
2024-03-15 14:06:10 -0400

    Updates to the README

--- app/management/commands/create_db.py
+++ app/management/commands/create_db.py
@@ -37,16 +37,17 @@ class Command(BaseCommand):
         -- COINS
         CREATE TABLE IF NOT EXISTS app_coin (
             id SERIAL PRIMARY KEY,
-            coin_id SERIAL,
             crypto_id VARCHAR(50) NOT NULL,
             name VARCHAR(50) NOT NULL,
             symbol VARCHAR(50) NOT NULL,
-            description VARCHAR DEFAULT 'None',
+            rank INTEGER,
+            market_cap_rank INTEGER,
+            description VARCHAR DEFAULT NULL,
             current_price DOUBLE PRECISION NOT NULL,
             market_cap DOUBLE PRECISION,
             ath DOUBLE PRECISION,
             atl DOUBLE PRECISION,
-            date_created DATE DEFAULT CURRENT_DATE,
+            date_created DATE DEFAULT NULL,
             image VARCHAR(200),
             price_history JSONB
         );
@@ -63,9 +64,62 @@ class Command(BaseCommand):
             volume NUMERIC(19, 4) NOT NULL,
             market_cap NUMERIC(19, 4) NOT NULL
         );
+
+        -- PlotImages
+        CREATE TABLE IF NOT EXISTS plot_images (
+            id SERIAL PRIMARY KEY,
+            user_id INTEGER REFERENCES accounts_customuser(id) ON DELETE CASCADE,
+            path VARCHAR(256) NOT NULL,
+            description TEXT,
+            date_created TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+            coin_id INTEGER REFERENCES app_coin(id) ON DELETE SET NULL
+        );
+
+        -- CsvUploads
+        CREATE TABLE IF NOT EXISTS csv_uploads (
+            id SERIAL PRIMARY KEY,
+            user_id INTEGER REFERENCES accounts_customuser(id) ON DELETE CASCADE,
+            path VARCHAR(256) NOT NULL,
+            description TEXT,
+            date_created TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+            coin_id INTEGER REFERENCES app_coin(id) ON DELETE SET NULL
+        );
+
+        -- GptResponses
+        CREATE TABLE IF NOT EXISTS gpt_responses (
+            id SERIAL PRIMARY KEY,
+            user_id INTEGER REFERENCES accounts_customuser(id) ON DELETE CASCADE,
+            path VARCHAR(256) NOT NULL,
+            description TEXT,
+            date_created TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+            coin_id INTEGER REFERENCES app_coin(id) ON DELETE SET NULL
+        );
+
+        CREATE TABLE IF NOT EXISTS platform (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(50) NOT NULL,
+            symbol VARCHAR(10) NOT NULL,
+            slug VARCHAR(50) NOT NULL,
+            token_address VARCHAR(100) NOT NULL
+        );
+
+        CREATE TABLE IF NOT EXISTS cryptodata (
+            id SERIAL PRIMARY KEY,
+            crypto_id INTEGER UNIQUE NOT NULL,
+            rank INTEGER NOT NULL,
+            name VARCHAR(50) NOT NULL,
+            symbol VARCHAR(10) NOT NULL,
+            slug VARCHAR(50) NOT NULL,
+            is_active BOOLEAN NOT NULL,
+            first_historical_data TIMESTAMP WITH TIME ZONE NOT NULL,
+            last_historical_data TIMESTAMP WITH TIME ZONE NOT NULL,
+            platform_id INTEGER REFERENCES platform(id)
+        );
+
+        ALTER TABLE accounts_customuser ADD COLUMN bio TEXT;
+        ALTER TABLE accounts_customuser ADD COLUMN profile_picture VARCHAR(255);
         """
 
         with connection.cursor() as cursor:
             cursor.execute(create_tables_sql)
             self.stdout.write(self.style.SUCCESS("Tables created successfully."))
-
